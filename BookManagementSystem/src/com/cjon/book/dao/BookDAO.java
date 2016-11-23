f887931d4f56103b7446d2464a19a99022ea48ab
gogo6725@naver.com <gogo6725@naver.com>
2016-11-23 20:03:33 +0900

    commit

--- BookManagementSystem/src/com/cjon/book/dao/BookDAO.java
+++ BookManagementSystem/src/com/cjon/book/dao/BookDAO.java
@@ -21,8 +21,9 @@ public class BookDAO {
 		ResultSet rs = null;
 		String result = null;
 		System.out.println(keyword);
+		
 		try {
-			String sql = "select bisbn, bimgbase64, btitle, bauthor, bprice " + "from book where btitle like ? limit " + start + ", 10";
+			String sql = "select bisbn, bimgbase64, btitle, bauthor, bprice from book where btitle like ?";
 			pstmt = con.prepareStatement(sql);
 			pstmt.setString(1, "%" + keyword + "%");
 			rs = pstmt.executeQuery();
@@ -36,8 +37,10 @@ public class BookDAO {
 				obj.put("author", rs.getString("bauthor"));
 				obj.put("price", rs.getString("bprice"));
 				arr.add(obj);
-				System.out.println(i++);
 			}
+			
+			System.out.println(" 리스트 출력 완료!!---------------------------------------------------");
+
 			result = arr.toJSONString();
 		} catch (Exception e) {
 			System.out.println(e);
@@ -48,43 +51,7 @@ public class BookDAO {
 		}
 		return result;
 	}
-
-	public boolean update(String isbn, String title, String author, String price) {
-		Connection con = DBTemplate.getConnection();
-		PreparedStatement pstmt = null;
-
-		boolean result = false;
-		try {
-			System.out.println(isbn);
-			System.out.println(title);
-			System.out.println(author);
-			System.out.println(price);
-			String sql = "update book set btitle=?, bauthor=?, bprice=? where bisbn=?";
-			pstmt = con.prepareStatement(sql);
-			pstmt.setString(1, title);
-			pstmt.setString(2, author);
-			pstmt.setInt(3, Integer.parseInt(price));
-			pstmt.setString(4, isbn);
-
-			int count = pstmt.executeUpdate();
-			// 결과값은 영향을 받은 레코드의 수
-			if (count == 1) {
-				result = true;
-				// 정상처리이기 때문에 commit
-				DBTemplate.commit(con);
-			} else {
-				DBTemplate.rollback(con);
-			}
-
-		} catch (Exception e) {
-			e.printStackTrace();
-		} finally {
-			DBTemplate.close(pstmt);
-			DBTemplate.close(con);
-		}
-		return result;
-	}
-
+	
 	public boolean delete(String isbn) {
 		Connection con = DBTemplate.getConnection();
 		PreparedStatement pstmt = null;
@@ -96,7 +63,9 @@ public class BookDAO {
 			String sql = "delete from book where bisbn=?";
 			pstmt = con.prepareStatement(sql);
 			pstmt.setString(1, isbn);
-
+			
+			System.out.println("삭제되었습니닷.---------------------------------------------------");
+			
 			int count = pstmt.executeUpdate();
 			// 결과값은 영향을 받은 레코드의 수
 			if (count == 1) {
@@ -113,162 +82,8 @@ public class BookDAO {
 			DBTemplate.close(pstmt);
 			DBTemplate.close(con);
 		}
-		return result;
-	}
-
-	public boolean insert(String img, String isbn, String title, String author, String translator, String supplement,
-			String publisher, String price, String date, String page) {
-
-		Connection con = DBTemplate.getConnection();
-		PreparedStatement pstmt = null;
-
-		boolean result = false;
-		try {
-			System.out.println("insert ");
-			System.out.println(img);
-			System.out.println(isbn);
-			System.out.println(title);
-			System.out.println(author);
-			System.out.println(translator);
-			System.out.println(supplement);
-			System.out.println(publisher);
-			System.out.println(price);
-			System.out.println(date);
-			System.out.println(page);
-
-			String sql = "insert into book (bisbn, btitle, bdate, bpage, bprice, bauthor, btranslator, bsupplement, bpublisher, bimgbase64) "
-					+ "values(?, ?, ?, ?, ?, ?, ?, ?, ?, ?)";
-			pstmt = con.prepareStatement(sql);
-
-			pstmt.setString(1, isbn);
-			pstmt.setString(2, title);
-			pstmt.setString(3, date);
-			pstmt.setString(4, page);
-			pstmt.setInt(5, Integer.parseInt(price));
-			pstmt.setString(6, author);
-			pstmt.setString(7, translator);
-			pstmt.setString(8, supplement);
-			pstmt.setString(9, publisher);
-			pstmt.setString(10, img);
-
-			int count = pstmt.executeUpdate();
-			// 결과값은 영향을 받은 레코드의 수
-			if (count == 1) {
-				result = true;
-				// 정상처리이기 때문에 commit
-				DBTemplate.commit(con);
-			} else {
-				DBTemplate.rollback(con);
-			}
-
-		} catch (Exception e) {
-			e.printStackTrace();
-		} finally {
-			DBTemplate.close(pstmt);
-			DBTemplate.close(con);
-		}
-		return result;
-
-	}
-
-	public String details(String isbn) {
-
-		Connection con = DBTemplate.getConnection();
-		PreparedStatement pstmt = null;
-		ResultSet rs = null;
-		String result = null;
-		try {
-			String sql = "select bdate, bpage, btranslator, bsupplement, bpublisher " + "from book where bisbn=?";
-			pstmt = con.prepareStatement(sql);
-			pstmt.setString(1, isbn);
-			rs = pstmt.executeQuery();
-			while (rs.next()) {
-				JSONObject obj = new JSONObject();
-				obj.put("date", rs.getString("bdate"));
-				obj.put("page", rs.getString("bpage"));
-				obj.put("translator", rs.getString("btranslator"));
-				obj.put("supplement", rs.getString("bsupplement"));
-				obj.put("publisher", rs.getString("bpublisher"));
-
-				result = obj.toString();
-			}
-			// result = obj.toString();//arr.toJSONString();
-		} catch (Exception e) {
-			e.printStackTrace();
-		} finally {
-			DBTemplate.close(rs);
-			DBTemplate.close(pstmt);
-			DBTemplate.close(con);
-		}
-
 		return result;
 	}
 	
-	public String info(String isbn) {
-
-		// Database처리가 나와요!
-		// 1. Driver Loading 
-		// 2. Connection 생성
-		Connection con = DBTemplate.getConnection();
-		PreparedStatement pstmt = null;
-		ResultSet rs = null;
-		String result = null;
-		System.out.println("info");
-		System.out.println(isbn);
-		JSONObject obj = new JSONObject();
-		try {
-			String sql = "select bisbn, bimgurl, btitle, bauthor, bprice " + "from book where bisbn=?";
-			pstmt = con.prepareStatement(sql);
-			pstmt.setString(1, isbn);
-			rs = pstmt.executeQuery();
-			// JSONArray arr = new JSONArray();
-			while (rs.next()) {
-
-				obj.put("isbn", rs.getString("bisbn"));
-				obj.put("img", rs.getString("bimgurl"));
-				obj.put("title", rs.getString("btitle"));
-				obj.put("author", rs.getString("bauthor"));
-				obj.put("price", rs.getString("bprice"));
-				// arr.add(obj);
-			}
-			result = obj.toJSONString();
-			System.out.println(result);
-		} catch (Exception e) {
-			e.printStackTrace();
-		} finally {
-			DBTemplate.close(rs);
-			DBTemplate.close(pstmt);
-			DBTemplate.close(con);
-		}
-		return result;
-
-	}
-
-	public String getListNum(String keyword) {
-		Connection con = DBTemplate.getConnection();
-		PreparedStatement pstmt = null;
-		ResultSet rs = null;
-		String result = null;
-		JSONObject obj = null;
-		try {
-			String sql = "select count(bisbn) from book where btitle like ?";
-			pstmt = con.prepareStatement(sql);
-			pstmt.setString(1, "%" + keyword + "%");
-			rs = pstmt.executeQuery();
-			while (rs.next()) {
-				obj = new JSONObject();
-				obj.put("num", rs.getString("count(bisbn)"));
-				
-			}
-			result = obj.toJSONString();
-		} catch (Exception e) {
-			System.out.println(e);
-		} finally {
-			DBTemplate.close(rs);
-			DBTemplate.close(pstmt);
-			DBTemplate.close(con);
-		}
-		return result;
-	}
 
 }
